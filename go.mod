d7024aed6168f7c5fa9e4fca712317f92198bb9a
rifqi-ridwan <rifqi.brd@gmail.com>
2021-02-09 15:10:35 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module multiplytable
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	t := template.Must(template.New("main").Funcs(template.FuncMap{"mul": Mul}).ParseGlob("templates/*"))
+
+	r.SetHTMLTemplate(t)
+
+	r.GET("/", func(c *gin.Context) {
+		data := []string{}
+		var x, y = 1, 1
+		for x <= 9 {
+			for y <= 9 {
+				data = append(data, fmt.Sprintf("%v x %v = %v", x, y, x*y))
+				y++
+			}
+			y = 1
+			x++
+		}
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"data": data})
+	})
+
+	log.Fatal(r.Run(":3000"))
+}
+
+func Mul(param1 int, param2 int) int {
+	return param1 * param2
+}
